e16176f03cb307bffe5bb552cb55c6056aed5261
Fengzhihao1108 <129584695+Fengzhihao1108@users.noreply.github.com>
2023-05-09 22:16:01 +0800

    修改界面提示

--- app/locales/cn.ts
+++ app/locales/cn.ts
@@ -1,10 +1,10 @@
 import { SubmitKey } from "../store/config";
 
 const cn = {
-  WIP: "该功能仍在开发中……",
+  WIP: "华强，这个功能还在开发中...",
   Error: {
     Unauthorized:
-      "欢迎来到由 豪 搭建的chatGPT-web站点！\n此刻访问密码不正确或为空，请前往[设置](/#/settings)页输入正确的访问密码，或者填入自己的 OpenAI API Key。",
+      "欢迎来到由 豪 搭建的ChatGPT-web站点！\n此刻访问密码不正确或为空，请前往[设置](/#/settings)页输入正确的访问密码，或者填入自己的 OpenAI API Key。",
   },
   ChatItem: {
     ChatItemCount: (count: number) => `${count} 条对话`,
